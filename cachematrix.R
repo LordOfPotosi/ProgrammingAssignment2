236fe1b3122a5912ba7da4865e0f7efa7202d371
LordOfPotosi <choudharyl2013@email.iimcal.ac.in>
2015-07-26 17:27:18 +0530

    Changes to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,26 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function returns an object as a list which is the get,set,getmean and setmean functions for the passed data matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
-   inv <- NULL
-
-   set <- function(y){
-
-      x <<- y
-      inv <<- NULL 
-
-   }
-
-   get <- function() x 
-
-   setinv <- function(inverse) inv <<- inverse
-
-   getinv <- function() inv
-
-   list(set = set, get = get, setinv = setinv, getinv = getinv)
+  
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y                                     # assign x the data in the parent frame
+    inv <<- NULL                                # assign null to the variable holding inverse
+    
+  }
+  
+  get <- function() x                           # get function to return the data x
+  
+  setinv <- function(inverse) inv <<- inverse   # setinv function to assign the value of inverse to inv
+  
+  getinv <- function() inv                      # return the value of inv, it will be null if not already cached
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv) # return list with functions as elements
+   
 
 }
 
@@ -28,22 +28,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
-   inv <- x$getinv()
-   
-   if(!is.null(inv)){
-     message("Using Cached Data")
-     return(inv)
-
-   } else {
-      data <- x$get()
-      inv <- solve(data,...)
-      x$setinv(inv)
-
-      
-   }
-   
-   inv
-
+  inv <- x$getinv()
+  
+  if(!is.null(inv)){
+    
+    message("Getting Cached Data")   # return cached value of inverse if already in cache
+    return(inv)
+    
+  } else {                           # if value not cached, need to get data and assign value of inverse using solve()    
+    data <- x$get()
+    inv <- solve(data,...)
+    x$setinv(inv)
+    
+  }
+  
+  inv
+  
+ 
 }
 
 
